eec2d2523afa230d3905252fd0b513a2a4a50387
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-06-23 19:52:49 -0500

    + import graphql cors and schema

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-const express = require('express');
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import cors from 'cors';
+import schema from './schema';
 
 const app = express();
+
+app.use(cors());
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphql: true
+	})
+);
 
 const port = process.env.PORT || 7500;
 
